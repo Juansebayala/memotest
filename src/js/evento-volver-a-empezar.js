e80d9191bd34738b0fad52aa07c55204b5f6915b
Juansebayala <107014615+Juansebayala@users.noreply.github.com>
2023-02-13 12:14:02 -0300

    Crea lógica para resetear juego

---
+++ src/js/evento-volver-a-empezar.js
@@ -0,0 +1,51 @@
+const $botonesReiniciar = document.querySelectorAll('.volver-a-empezar');
+
+$botonesReiniciar.forEach(function($boton){
+  $boton.onclick = resetearJuego;
+});
+
+function resetearJuego() {
+  resetearTiempoJugado();
+  resetearIntentosRealizados();
+  $cartas = document.querySelectorAll('.carta');
+  mostrarCartas($cartas);
+  desabilitarClicCartas($cartas);
+  habilitarBotonJugar();
+}
+
+function habilitarBotonJugar() {
+  $botonJugar.onclick = comenzarJuego;
+}
+
+function mostrarCartas($cartas) {
+  $cartas.forEach(function($carta) {
+    ocultarImagenCarta($carta);
+    setTimeout(function() {
+      $carta.style.opacity = '1';
+      $carta.style.visibility = 'visible';
+    }, 300);
+  });
+}
+
+function ocultarImagenCarta($carta) {
+  $carta.childNodes[1].style.opacity = "0";
+}
+
+function desabilitarClicCartas($cartas) {
+  $cartas.forEach(function($carta) {
+    $carta.onclick = function() {};
+  })
+}
+
+function resetearTiempoJugado() {
+  finTiempoJugado = true;
+  const RETRASO_PARAR_TIEMPO = 700;
+  setTimeout(function() {
+    document.querySelector("#tiempo-jugado").textContent = '00:00';
+  }, RETRASO_PARAR_TIEMPO);
+}
+
+function resetearIntentosRealizados() {
+  intentosRealizados = 0;
+  document.querySelector("#intentos").textContent = '0';
+}
